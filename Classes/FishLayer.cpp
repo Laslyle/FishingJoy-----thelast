713fbb13fcbfbe6115cae523da9d2fcedc894a15
Laslyle <30777117+Laslyle@users.noreply.github.com>
2017-11-19 10:32:36 +0800

    时间种子

--- Classes/FishLayer.cpp
+++ Classes/FishLayer.cpp
@@ -3,6 +3,7 @@
 #define FISH_MAX_COUNT 50
 
 
+
 FishLayer::FishLayer(void)
 {
 }
@@ -23,10 +24,12 @@ bool FishLayer::init()
 	_fishes = CCArray::createWithCapacity(FISH_MAX_COUNT);
 
 	CC_SAFE_RETAIN(_fishes);
+	srand(time(0));
 	for (int i = 0; i < FISH_MAX_COUNT; i++)
 	{
-		//int type = CCRANDOM_0_1() * k_Fish_Type_Count;
-		int type= CCRANDOM_0_1() * (k_Fish_Type_Count-1)+1;
+		
+		//int type = rand()%100/(double)101 * k_Fish_Type_Count;
+		int type= (rand()%100/(double)101)* (k_Fish_Type_Count-1)+1;
 		CCLOG("FishLayer type=%d",type);
 		Fish *fish = Fish::create((FishType)type);
 		_fishes->addObject(fish);
@@ -51,8 +54,9 @@ bool FishLayer::init()
 
 void FishLayer::addFish(float delta)
 {
-	int addToCount = CCRANDOM_0_1() * 5 + 1;
+	int addToCount = (rand()%100/(double)101) * 5 + 1;
 	int count = 0;
+	srand(time(0));
 	CCObject *obj;
 	CCSize winSize = CCDirector::sharedDirector()->getWinSize();
 	CCARRAY_FOREACH(_fishes, obj)
@@ -63,7 +67,8 @@ void FishLayer::addFish(float delta)
 			continue;
 		}
 		count++;
-		fish->setPosition(ccp(CCRANDOM_0_1()*winSize.width, CCRANDOM_0_1()*winSize.height));
+		
+		fish->setPosition(ccp((rand()%100/(double)101)*winSize.width, rand()%100/(double)101*winSize.height));
 		this->addChild(fish);
 		if (count == addToCount)
 		{
